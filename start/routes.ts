8450cf4128d42ac76024ae0d9a1b58ee9d326d90
razordice <reza.fauzan1991@gmail.com>
2025-06-12 15:17:33 +0700

    add jwt crud API and login

--- start/routes.ts
+++ start/routes.ts
@@ -11,18 +11,28 @@ import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js';
 import ApiProductsController from '#controllers/api/api_products_controller';
 import ProductsController from '#controllers/products_controller';
+import ApiAuthController from '#controllers/api/api_auth_controller';
 
-router.group(() => {
-    router.get('/products', [ApiProductsController, 'index'])
-    router.post('/products', [ApiProductsController, 'store'])
-    router.get('/products/:id', [ApiProductsController, 'show'])
-    router.put('/products/:id', [ApiProductsController, 'update'])
-    router.delete('/products/:id', [ApiProductsController, 'destroy'])
-}).use(middleware.api()).prefix('/api')
-
-// router.on('/products').render('pages/home')
 
 router.group(() => {
     router.resource('products', ProductsController)
 }).use(middleware.csrf())
 
+router.group(() => {
+})
+
+// Protected routes
+router.group(() => {
+    router.post('/register', [ApiAuthController, 'register'])
+    router.post('/login', [ApiAuthController, 'login'])
+    
+    router.group(() => {
+        router.get('/profile', [ApiAuthController, 'profile'])
+        router.get('/products', [ApiProductsController, 'index'])
+        router.post('/products', [ApiProductsController, 'store'])
+        router.get('/products/:id', [ApiProductsController, 'show'])
+        router.put('/products/:id', [ApiProductsController, 'update'])
+        router.delete('/products/:id', [ApiProductsController, 'destroy'])
+    }).use(middleware.auth())
+    // Product routes dengan JWT
+}).prefix('/api').use(middleware.api())
